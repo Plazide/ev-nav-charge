d1d3b57ab67b1119fb75d5b2e7663e6f5e4b2572
Carl Hallén Jansson <plazide@gmail.com>
2023-06-25 15:20:17 +0200

    Fix build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import Map from "../components/Map";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("@/components/Map"));
 
 export default function Home() {
   if (typeof window === "undefined") return null;
